25c5295890b672237eb35e5f30c61d8ab4da51b3
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-22 14:08:23 -0500

    done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    attr_accessor :species, :nickname, :weight, :zoo
+
+    @@all = []
+    def initialize(species, nickname, weight)
+        @species = species
+        @nickname = nickname
+        @weight = weight
+        @zoo
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        #returns all the animals that are that species
+        all.select {|animals| animals.species == species}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,24 @@ require 'pry'
 
 #Test your code here
 
+zoo1 = Zoo.new("Bronx Zoo", "Bronx")
+zoo2 = Zoo.new("Queens Zoo", "Queens")
+zoo3 = Zoo.new("Queens Aquarium", "Queens")
+
+animal1 = Animal.new("gorrila", "Harambe", 250)
+animal2 = Animal.new("orca", "Shamu", 500)
+animal3 = Animal.new("dog", "Snoopy", 80)
+animal4 = Animal.new("dog", "Pluto", 130)
+animal5 = Animal.new("dog", "Scooby", 180)
+animal6 = Animal.new("parrot", "Tucan", 40)
+
+animal1.zoo = zoo1
+animal2.zoo = zoo3
+animal3.zoo = zoo2
+animal4.zoo = zoo2
+animal5.zoo = zoo1
+animal6.zoo = zoo2
+
 
 binding.pry
 puts "done"
